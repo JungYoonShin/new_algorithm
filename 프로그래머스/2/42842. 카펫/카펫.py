8cfa4b90a5641fa33208d54401af1a865e1f9c86
정윤 <sju08227@naver.com>
2025-04-15 13:53:24 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -2,13 +2,15 @@ import math
 def solution(brown, yellow):
     answer = []
     
-    total = brown + yellow
     
-    for a in range(1, total+1):
-        if total % a == 0:
+    total = brown + yellow
+    for i in range(1, int(math.sqrt(total))+1):
+        if total % i == 0:
+            a = total // i
             b = total // a
-            if a >= b:
-                if 2*a + 2*b - 4 == brown and (a-2) * (b-2) == yellow:
+            
+            if (a-2) * (b-2) == yellow:
+                if 2*a +2*(b-2) == brown:
                     return [a, b]
-                    
-                    
+    
+    return answer
