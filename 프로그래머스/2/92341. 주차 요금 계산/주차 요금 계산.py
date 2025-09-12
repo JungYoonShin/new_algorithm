59fb13479d0074304e5f1429365d9fc0b1e358f5
정윤 <sju08227@naver.com>
2025-09-12 09:44:24 +0900

    [level 2] Title: 주차 요금 계산, Time: 5.27 ms, Memory: 9.71 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,39 +1,37 @@
-from collections import defaultdict
 import math
+from collections import defaultdict
 def solution(fees, records):
     answer = []
+    timetable = defaultdict(list)
     
-    dic = defaultdict(list)
-    sum_time = defaultdict(int)
     for record in records:
-        time, number, out_or_in = record.split()
-        number = int(number)
-        hour, minute = map(int, time.split(":"))
+        time, num, in_or_out = record.split(" ")
+        timetable[num].append(time)
+    
+    
+    for a, b in timetable.items():
+        if not len(b) % 2 == 0:
+            timetable[a].append('23:59')
+    
+    total = defaultdict(int)
+    
+    for a, b in timetable.items():
+        times = 0
+        for i in range(1, len(b), 2):
+            in_hour, in_minute = map(int, b[i-1].split(":"))
+            out_hour, out_minute = map(int, b[i].split(":"))
+            
+            times += (out_hour * 60 + out_minute) - (in_hour * 60 + in_minute)
+        total[a] = times
+            
+    for num, time in total.items():
+        if time <= fees[0]:
+            cost = fees[1]
+        else: 
+            cost = fees[1] + math.ceil((time-fees[0])/fees[2]) * fees[3]
+        total[num] = cost
         
-        #입차기록이 있다면 출차시각 - 입차시각
-        if dic[number]:
-            in_hour, in_minute = map(int, dic[number][0].split(":"))
-            total = (hour * 60 + minute)-(in_hour * 60 + in_minute) 
-            sum_time[number] += total
-            dic[number].pop()
-            
-        #입차기록이 없다면 입차시각 저장
-        else:
-            dic[number].append(time)
-    
-    for k in dic.keys():
-        if dic[k]:
-            in_hour, in_minute = map(int, dic[k][0].split(":"))
-            sum_time[k] += (23 * 60 + 59)-(in_hour * 60 + in_minute) 
-            
-    basic_time, basic_fee, time, fee = fees
-    sum_time = dict(sorted(sum_time.items()))
-    
-    for k in sum_time.keys():
-        if sum_time[k] > basic_time:
-            total_fee = basic_fee + math.ceil((sum_time[k]-basic_time)/time) * fee
-            answer.append(total_fee)
-        else:
-            answer.append(basic_fee)
-    
-    return answer
+    answer = [[num, cost] for num, cost in total.items()]
+    answer.sort(key = lambda x: x[0])
+
+    return [x[1] for x in answer]
