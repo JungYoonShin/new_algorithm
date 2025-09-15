b7d7015923d6f80ec84bdaadb31db4f01baf240e
정윤 <sju08227@naver.com>
2025-09-15 23:03:42 +0900

    [level 3] Title: 불량 사용자, Time: 147.60 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,26 +1,34 @@
-from itertools import permutations
+from collections import defaultdict
 def solution(user_id, banned_id):
     answer = 0
     
-    can = [[] for _ in range(len(banned_id)+1)] 
+    ban = defaultdict(list)
+    for i in range(len(banned_id)):
+        for user in user_id:
+            flag = True
+            if len(user) == len(banned_id[i]):
+                for j in range(len(banned_id[i])):
+                    if banned_id[i][j] != '*':
+                        if user[j] != banned_id[i][j]:
+                            flag = False
+                            break
+            else:
+                flag = False
+                        
+            if flag:
+                ban[i].append(user)
     
-    def canbe(user_tuple, banned_id):
-        for u, b in zip(user_tuple, banned_id):
-            if len(u) != len(b):
-                return False
-            for i in range(len(u)):
-                if b[i] != '*' and u[i] != b[i]:
-                    return False
-        return True
-
+    result = set()
+    def dfs(idx, chosen):
+        if idx == len(banned_id):
+            result.add(frozenset(chosen))
+            return
+        
+        for user in ban[idx]:
+            if user not in chosen:
+                chosen.append(user)
+                dfs(idx+1, chosen)
+                chosen.remove(user)
     
-    user_permutation = list(permutations(user_id, len(banned_id)))
-    
-    ban_set = []
-    
-    for user in user_permutation:
-        if canbe(user, banned_id):
-            if set(user) not in ban_set:
-                ban_set.append(set(user))    
-    
-    return len(ban_set)
+    dfs(0, [])
+    return len(result)
