84548ab5defb5864269b09c6cfbe70d0156f9b50
정윤 <sju08227@naver.com>
2025-09-15 22:14:13 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.04 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,42 +1,23 @@
 def solution(dartResult):
-    answer = 0
-    
-    dart = []
-    result = ''
-    
-    dartResult = dartResult.replace('10', 'N')
-
-    for i in range(len(dartResult)-1):
-        result += dartResult[i]
-        if dartResult[i+1].isdigit() or dartResult[i+1] == 'N':
-            dart.append(result)
-            result = ''
-    dart.append(result + dartResult[i+1:])
-    
     score = [0]
     area_score = {'S': 1, 'D': 2, 'T': 3}
     
-    i=1
-    for d in dart:
-        if len(d) == 3:
-            n, area, award = d
-            if n == 'N':
-                n = 10
-            n = int(n)
-            if award == '*':
-                score[i-1] = score[i-1] * 2
-                score.append((n ** area_score[area]) * 2)
-
-            else:
-                score.append(-1 * (n ** area_score[area]))
-        else:
-            n, area = d
-            if n == 'N':
-                n = 10
-            n = int(n)
-            score.append(n ** area_score[area])
-                
-        i+=1
+    s = ''
+    for d in dartResult:
+        if d.isdigit():
+            s += d
+        
+        elif d in area_score:  # 보너스 S/D/T
+            n = int(s)
+            score.append(n ** area_score[d])
+            s = ''
+        
+        elif d == '*':
+            if score:
+                score[-1] *= 2
+                score[-2] *= 2
+        elif d == '#':
+            if score:
+                score[-1] *= -1
     
-
     return sum(score)
