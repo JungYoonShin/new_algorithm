b90dbb59757f3ab8c5952eddbbf4834d8fb7b7db
정윤 <sju08227@naver.com>
2025-09-16 22:39:59 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 3.11 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -1,24 +1,39 @@
 def solution(n, t, m, timetable):
     answer = ''
+    
+    #아직 넉넉하다면 버스 시각 맞춰서, 안넉넉하면 젤 마지막 애보다 1분 빨리
+    start = 9 * 60
+    
+    arrive = {start: []}
+    for i in range(n-1):
+        start += t
+        arrive[start] = []
+    
     timetable.sort()
-    bus_time = {}
-    start_time = 9 * 60
-    for i in range(n):
-        bus_time[start_time + t * i] = []
-    
+        
     for time in timetable:
-        hour, minute = time.split(":")
-        total_time = int(hour) * 60 + int(minute)
-        for bus in bus_time.keys():
-            if total_time <= bus and len(bus_time[bus]) < m:
-                bus_time[bus].append(total_time)
-                break
+        hour, minute = map(int, time.split(":"))
+        for k in arrive.keys():
+            if hour == 23 and minute == 59:
+                continue
+            else:
+                if (hour*60+minute) <= k and len(arrive[k]) < m:
+                    arrive[k].append(hour*60+minute)
+                    break
     
-    last_bus_time = list(bus_time)[-1]
-    if len(bus_time[last_bus_time]) < m:
-        return f"{last_bus_time//60:02d}:{last_bus_time%60:02d}"
+    last_time = list(arrive.keys())[-1]
+    print(arrive)
+    
+    if len(arrive[last_time]) < m:
+        hour = last_time // 60
+        minute = last_time % 60
     else:
-        t = bus_time[last_bus_time][-1] - 1
-        return f"{t//60:02d}:{t%60:02d}"
+        hour = (arrive[last_time][-1]-1) // 60
+        minute = (arrive[last_time][-1]-1) % 60
     
-    return answer
+    hour, minute = str(hour), str(minute)
+    
+    hour = hour.zfill(2)
+    minute = minute.zfill(2)
+    
+    return f"{hour}:{minute}"
