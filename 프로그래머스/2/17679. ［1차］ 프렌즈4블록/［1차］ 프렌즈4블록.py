fc2f1d47f8b5967023c1a6b3eb5b706e61749173
정윤 <sju08227@naver.com>
2025-09-19 00:50:31 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 65.43 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -2,33 +2,37 @@ def solution(m, n, board):
     answer = 0
     board = [list(row) for row in board]
 
+    def delete(board, r, c):
+        col = []
+        
+        for i in range(c):
+            col_list = [board[j][i] for j in range(r) if board[j][i] != '-']
+            
+            for j in range(r-1, -1, -1):
+                if col_list:
+                    board[j][i] = col_list.pop()
+                else:
+                    board[j][i] = '*'
+        return board
+        
     while True:
         should_delete = set()
 
         # 2x2 같은 블록 찾기
         for i in range(m-1):
             for j in range(n-1):
-                if board[i][j] != '-' and \
+                if board[i][j] not in ['-', '*'] and \
                    board[i][j] == board[i+1][j] == board[i][j+1] == board[i+1][j+1]:
                     should_delete.update({(i, j), (i+1, j), (i, j+1), (i+1, j+1)})
 
-        if not should_delete:  # 더 이상 지울 게 없으면 종료
+        if not should_delete:
             break
 
-        # 지우기
         for (x, y) in should_delete:
             board[x][y] = '-'
         answer += len(should_delete)
 
         # 블록 내리기
-        for j in range(n):
-            empty = ['-'] * m
-            idx = m-1
-            for i in range(m-1, -1, -1):
-                if board[i][j] != '-':
-                    empty[idx] = board[i][j]
-                    idx -= 1
-            for i in range(m):
-                board[i][j] = empty[i]
+        board = delete(board, m, n)
 
     return answer
