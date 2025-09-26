08762f2e3cd2114a9feb6aca3122012bf446c849
정윤 <sju08227@naver.com>
2025-09-26 20:35:29 +0900

    [level 3] Title: 합승 택시 요금, Time: 1214.47 ms, Memory: 15.2 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,44 +1,40 @@
 import heapq
 def solution(n, s, a, b, fares):
-    answer = 0
+    INF = 1e9
     
     graph = [[] for _ in range(n+1)]
-    
     for fare in fares:
-        x, y, z  = fare
-        graph[x].append((y, z))
-        graph[y].append((x, z))
-    
-
+        graph[fare[0]].append([fare[1], fare[2]])
+        graph[fare[1]].append([fare[0], fare[2]])
+        
+        
     def djk(start):
+        dist = [INF] * (n+1)
+        dist[start] = 0
         q = []
         heapq.heappush(q, (0, start))
-        dist = [1e9] * (n+1)
-        dist[start] = 0
-
-        while q:
-            cost, u = heapq.heappop(q)
-
-            if dist[u] < cost:
-                continue
-
-            for v, w in graph[u]:
-                if dist[v] > dist[u] + w:
-                    dist[v] = cost + w
-                    heapq.heappush(q, (dist[v], v))
-    
-        return dist
-    
-    #s번에서 i번까지 동승 그리고 i번에서 최소거리로 각자집까지 이동
-    dist_s = djk(s)
-    dist_a = djk(a)
-    dist_b = djk(b)
-    
-    min_cost = 1e9
-    for i in range(1, n+1):
-        total = dist_s[i] + dist_a[i] + dist_b[i]
-        if total < min_cost:
-            min_cost = total
         
+        while q:
+            cost, v = heapq.heappop(q)
+            if dist[v] < cost:
+                continue
+            
+            for togo, w in graph[v]:
+                if dist[v] + w < dist[togo]:
+                    dist[togo] = dist[v] + w
+                    heapq.heappush(q, [dist[togo], togo])
+        return dist
+
     
-    return min_cost
+    together = djk(s)
+    alone_a = together[a]
+    alone_b = together[b]
+    
+    answer = alone_a + alone_b
+    for i in range(1, n+1):
+        #i까지 합승을 하는 거임, 합승을 안할 수도 잇음..(각자 가는게 더 빠르다면)
+        alone = djk(i)
+        total = together[i] + alone[a] + alone[b]
+        answer = min(total, answer)
+                
+    return answer
