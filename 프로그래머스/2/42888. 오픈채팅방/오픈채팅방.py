60e56ff762818d548992ad862b6ae8f280b4cce7
정윤 <sju08227@naver.com>
2025-09-29 22:26:48 +0900

    [level 2] Title: 오픈채팅방, Time: 144.23 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,19 +1,19 @@
 from collections import defaultdict
-def solution(record):
+def solution(records):
     answer = []
-    dict = defaultdict(list)
     
-    for r in record:
-        status = r.split()
-        if status[0] != 'Leave':
-            dict[status[1]] = status[2]
+    name = defaultdict(int)
     
-    for r in record:
-        status = r.split()
-        if status[0] == 'Enter':
-            answer.append(dict[status[1]] + "님이 들어왔습니다.")
-        elif status[0] == 'Leave':
-            answer.append(dict[status[1]] + "님이 나갔습니다.")
-            
-    
+    for record in records:
+        s = record.split(" ")
+        if s[0] == 'Leave':
+            answer.append([s[1], "나갔습니다."])
+        elif s[0] == 'Enter':
+            name[s[1]] = s[2]
+            answer.append([s[1], "들어왔습니다."])
+        else:
+            name[s[1]] = s[2]
+        
+    for i in range(len(answer)):
+        answer[i] = name[answer[i][0]] + "님이 " + answer[i][1]
     return answer
