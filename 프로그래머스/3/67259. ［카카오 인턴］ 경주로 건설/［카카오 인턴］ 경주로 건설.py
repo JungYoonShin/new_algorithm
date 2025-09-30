24e7ad065bcb63021c1b4f3e14279fdeafa6962c
정윤 <sju08227@naver.com>
2025-09-30 16:58:50 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 62.61 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,38 +1,37 @@
 from collections import deque
+answer = 1e9
 
 def solution(board):
-    n = len(board)
+    
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-    INF = 1e9
+    
+    n = len(board)
+    
+    visited = [[False for i in range(n)] for _ in range(n)]
+    graph = [[1e9 for _ in range(n)] for _ in range(n)]
 
-    # graph[x][y][dir] = (x,y)에 dir 방향으로 들어왔을 때 최소 비용
-    graph = [[[INF]*4 for _ in range(n)] for _ in range(n)]
-    q = deque()
-
-    # 시작점은 모든 방향 가능
-    for i in range(4):
-        graph[0][0][i] = 0
-    q.append((0, 0, -1, 0))  # (x, y, 방향, 비용)
-
-    answer = INF
-    while q:
-        a, b, d, cost = q.popleft()
-
-        if a == n-1 and b == n-1:
-            answer = min(answer, cost)
-            continue
-
-        for i in range(4):
-            nx, ny = a + dx[i], b + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 0:
-                if d == -1 or d == i:  
-                    new_cost = cost + 100
-                else: 
-                    new_cost = cost + 600
-
-                if new_cost < graph[nx][ny][i]:
-                    graph[nx][ny][i] = new_cost
-                    q.append((nx, ny, i, new_cost))
-
-    return answer
+    def bfs(d):
+        q = deque([(0, 0, d, 0)])
+        graph[0][0] = 0
+        
+        while q:
+            a, b, direction, cost = q.popleft()
+            
+            for i in range(4):
+                nx = a + dx[i]
+                ny = b + dy[i]
+                
+                if 0<=nx<n and 0<=ny<n:
+                    if board[nx][ny] == 0:
+                        if direction == i:
+                            new_cost = cost + 100
+                        if direction != i:
+                            new_cost = cost + 600
+                        if new_cost <= graph[nx][ny]:
+                            graph[nx][ny] = new_cost
+                            q.append((nx, ny, i, new_cost))
+        
+        return graph[n-1][n-1]
+    
+    return min(bfs(3), bfs(1))
