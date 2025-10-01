d95584e7d1062664c3bf7754028f24aedbf799e7
정윤 <sju08227@naver.com>
2025-10-02 01:09:44 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 10.26 ms, Memory: 15.5 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -1,7 +1,10 @@
 from collections import deque
 import copy
+import sys
+sys.setrecursionlimit(10**8)
+
+answer = ''
 def solution(n, m, x, y, r, c, k):
-    answer = ''
     
     x -=1 
     y -= 1
@@ -10,41 +13,30 @@ def solution(n, m, x, y, r, c, k):
     
     #이동거리 총 k여야함, 그리고 같은 격자 2번 이상 방문 가능, 모든 경로중 문자열 사전 순으로 가장 빠른 경로
     
-    
     dx = [1, 0, 0, -1]
     dy = [0, -1, 1, 0]
-    def bfs(start_x, start_y):
-        all_move = []
+    def dfs(start_x, start_y, depth, move):
+        global answer
+        if depth == k:
+            if start_x == r and start_y == c:
+                return move
 
-        q = deque([(start_x, start_y, 0, "")])
-        
-        while q:
-            a, b, depth, move = q.popleft()
+        for i in range(4):
+            nx = start_x + dx[i]
+            ny = start_y + dy[i]
             
-            if k - depth < abs(a - r) + abs(b - c):
-                continue
-            
-            if depth == k:
-                if a==r and b==c:
-                    move = move.replace('0', "d").replace('1', "l").replace('2', "r").replace('3', "u")
-                    return move
-            
-            for i in range(4):
-                nx = a + dx[i]
-                ny = b + dy[i]
-                
-                if 0<=nx<n and 0<=ny<m:
-                    if abs(nx - r) + abs(ny - c) > k - depth - 1:
-                        continue
-                    q.append((nx, ny, depth+1, move+str(i)))
-                    break
-                        
-    dist = abs(x - r) + abs(y - c)
-    if dist > k:
-        return "impossible"
+            if 0 <= nx < n and 0 <= ny < m:
+                remain = k - (depth + 1)
+                dist = abs(nx - r) + abs(ny - c)
+                if dist > remain or (remain - dist) % 2 == 1:
+                    continue
+                                    
+                res = dfs(nx, ny, depth+1, move+str(i)) 
+                if res is not None:
+                    return res
     
-    result = bfs(x, y)
-    if not result:
+    mm = dfs(x, y, 0, "")
+    if mm is None:
         return "impossible"
-    
-    return result
+    mm = mm.replace('0', "d").replace('1', "l").replace('2', "r").replace('3', "u")
+    return mm
