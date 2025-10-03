e93f2a2018a8ae326fb2343da37484d0f6220fe8
정윤 <sju08227@naver.com>
2025-10-03 15:59:38 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.20 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,30 +1,26 @@
-import copy
 def solution(n, wires):
     answer = 1e9
-    def dfs(start, visited, graph):
-        cnt = 1
-        visited[start] = True
-        for i in graph[start]:
-            if not visited[i]:
-                cnt += dfs(i, visited, graph)
-        
-        return cnt
-
     
-    graph = [[] for _ in range(n + 1)]
+    def dfs(x, not_a, not_b):
+        visited[x] = True
+        visit_set.add(x)
+        for v in graph[x]:
+            if (x == not_a and v == not_b) or (x == not_b and v == not_a):
+                continue
+            if not visited[v]:
+                dfs(v, not_a, not_b)
+    
+    graph = [[] for _ in range(n+1)]
     for wire in wires:
         a, b = wire
         graph[a].append(b)
         graph[b].append(a)
     
-    #하나 연결선 제거
     for wire in wires:
-        a, b = wire
-        tmp = copy.deepcopy(graph)
-        tmp[a].remove(b)
-        tmp[b].remove(a)
-        visited = [0] * (n+1)
-        s = dfs(a, visited, tmp)
-        answer = min(answer, abs((n-s) - s))
+        visited = [False] * (n+1)
+        visit_set = set()
+        dfs(1, wire[0], wire[1])
+        answer = min(answer, abs(len(visit_set) - (n - len(visit_set))))
         
-    return answer 
+    
+    return answer
