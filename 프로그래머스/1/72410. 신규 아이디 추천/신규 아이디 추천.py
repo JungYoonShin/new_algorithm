59f2b84ddb98a69e188a7234f0b909adc2860a73
정윤 <sju08227@naver.com>
2025-10-03 15:07:00 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.63 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,36 +1,38 @@
 def solution(new_id):
-    # 1단계: 모든 대문자를 소문자로 치환
+    answer = ''
+    
+    #1단계 소문자로 치환
     new_id = new_id.lower()
     
-    # 2단계: 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 문자 제거
-    valid_chars = ['-', '_', '.']
-    answer = ''
+    #2단계 알파벳 소문자, 숫자, -, _, .를 제외한 모든 문자 제거
+    new = ''
     for s in new_id:
-        if s.isalnum() or s in valid_chars:
-            answer += s
+        if s.isalpha() or s.isdigit() or s in['-', '_', '.']:
+            new += s
     
-    # 3단계: 연속된 마침표를 하나로 치환
-    while '..' in answer:
-        answer = answer.replace('..', '.')
+    #3단계 .가 2번 연속 이상인 부분을 .로 치환
+    for i in range(len(new)):
+        new = new.replace('..', '.')
     
-    # 4단계: 처음과 끝의 마침표 제거
-    if answer and answer[0] == '.':
-        answer = answer[1:]
-    if answer and answer[-1] == '.':
-        answer = answer[:-1]
+    #4단계 .가 처음이나 끝에 위치하면 제거
+    if new and new[0] == '.':
+        new = new[1:]
+    if new and new[-1] == '.':
+        new = new[0:len(new)-1]
+        
+    #5단계 빈 문자열이라면 a를 대입
+    if not new:
+        new = 'a'
     
-    # 5단계: 아이디가 비어 있으면 'a'를 대입
-    if len(answer) == 0:
-        answer += 'a'
+    #6단계 16자 이상이면 15개만 남기고 제거, 제거 후 마침표가 마지막에 오면 제거
+    if len(new) >= 16:
+        new = new[0:15]
+        if new[-1] == '.':
+            new = new[0:14]
     
-    # 6단계: 아이디 길이가 16자 이상이면 15자까지 자르고 마지막 마침표 제거
-    if len(answer) >= 16:
-        answer = answer[:15]
-        if answer[-1] == '.':
-            answer = answer[:-1]
-    
-    if len(answer) <= 2:
-        while len(answer) < 3:
-            answer += answer[-1]
-    
-    return answer
+    #7단계 길이가 2자 이하라면 마지막 문자를 길이 3이 될때까지 붙이기
+    if len(new) <= 2:
+        while len(new) <= 2:
+            new += new[-1]
+    # print(new)
+    return new
