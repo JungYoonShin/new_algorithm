2d2113a63b4c2fc1a9ea4ccd47441059e2ccf6e7
정윤 <sju08227@naver.com>
2025-10-04 20:35:29 +0900

    [level 3] Title: 양과 늑대, Time: 24.15 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,21 +1,20 @@
+import copy
 def solution(info, edges):
     answer = -1e9
     
-    #양 <= 늑대 이면 안됨, 최대한 많은 수의 양을 모아서 돌아와야함
     n = len(info)
     graph = [[] for _ in range(n)]
-    for edge in edges:
-        a, b = edge
+    for a, b in edges:
         graph[a].append(b)
     
     def dfs(now, sheep, wolf, can_visit):
         nonlocal answer
         answer = max(answer, sheep)
 
-        can_visit.update(graph[now][:])
         if sheep <= wolf:
             return
-
+        
+        can_visit.extend(graph[now][:])
         for v in can_visit:
             s, w = sheep, wolf
             if info[v] == 1:
@@ -25,11 +24,10 @@ def solution(info, edges):
             if s <= w:
                 continue
 
-            next_visit = set(can_visit)
+            next_visit = copy.deepcopy(can_visit)
             next_visit.remove(v)
             dfs(v, s, w, next_visit)
 
-    
-    dfs(0, 1, 0, set())
-    print(answer)
+
+    dfs(0, 1, 0, [])
     return answer
