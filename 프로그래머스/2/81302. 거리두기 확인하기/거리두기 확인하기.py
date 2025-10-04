442d101fe61d5c277abaf6a240da19910ccbcc2a
정윤 <sju08227@naver.com>
2025-10-04 21:58:43 +0900

    [level 2] Title: 거리두기 확인하기, Time: 2.51 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -0,0 +1,38 @@
+def solution(places):
+    answer = [1] * 5
+    n = 5
+    
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
+    def dfs(idx, x, y, depth, visited):
+        if depth >= 1:
+            if board[x][y] == 'P':
+                answer[idx] = 0
+        if depth >= 2:
+            return
+            
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0<=nx<n and 0<=ny<n:
+                if not visited[nx][ny] and board[nx][ny] != 'X':
+                    visited[nx][ny] = True
+                    dfs(idx, nx, ny, depth+1, visited)
+                    visited[nx][ny] = False
+                    
+    
+    for idx, place in enumerate(places):
+        board = list(map(list, place))
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 'P':
+                    visited = [[False for _ in range(n)] for _ in range(n)]
+                    visited[i][j] = True
+                    dfs(idx, i, j, 0, visited)
+        # print(answer)
+        # print()
+    
+    return answer
