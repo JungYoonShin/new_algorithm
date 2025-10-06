4aa1ea8a729286474e2cb98eecd39bc6f6081715
정윤 <sju08227@naver.com>
2025-10-06 20:50:05 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 5.11 ms, Memory: 9.84 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -10,10 +10,11 @@ def solution(files):
             if f.isdigit():
                 if head_end == -1:
                     head_end = i-1
-                number_end = i
                 number_cnt += 1
-            elif number_cnt != 0:
-                    break
+                number_end = i
+                
+            elif number_cnt!= 0: 
+                break
         
         head = file[0:head_end+1].lower()
         number = file[head_end+1:number_end+1]
@@ -22,4 +23,5 @@ def solution(files):
         after_file.append([head, int(number), idx, file])
     
     after_file.sort(key = lambda x: (x[0], x[1], x[2]))
+    print(after_file)
     return [x[3] for x in after_file]
