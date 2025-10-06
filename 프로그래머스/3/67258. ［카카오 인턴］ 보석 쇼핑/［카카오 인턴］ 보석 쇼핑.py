1e7d1d8c8cbcacbd3f5a0a5cf1b99d7f46bf4b02
정윤 <sju08227@naver.com>
2025-10-06 15:36:17 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 72.86 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,27 +1,28 @@
 from collections import defaultdict
-
 def solution(gems):
-    gem_type = len(set(gems))
-    n = len(gems)
+    answer = []
     
-    gem_cnt = defaultdict(int)
+    find_gem = defaultdict(int)
+    start = 0
+    n = len(gems)
+    gem_type = len(set(gems))
+    answer = [0, n-1]
     unique = 0
-    answer = (0, n - 1)
     end = 0
-
+    
     for start in range(n):
-        while end < n and unique < gem_type:
-            gem_cnt[gems[end]] += 1
-            if gem_cnt[gems[end]] == 1:
+        while unique < gem_type and end < n:
+            find_gem[gems[end]] += 1
+            if find_gem[gems[end]] == 1:
                 unique += 1
             end += 1
-
+        
         if unique == gem_type:
-            if end - start - 1 < answer[1] - answer[0]:
-                answer = (start, end - 1)
-
-        gem_cnt[gems[start]] -= 1
-        if gem_cnt[gems[start]] == 0:
+            if end-1-start < answer[1] - answer[0]:
+                answer= [start, end-1]
+        
+        find_gem[gems[start]] -=1
+        if find_gem[gems[start]] == 0:
             unique -= 1
-
-    return [answer[0] + 1, answer[1] + 1]
+    
+    return [answer[0]+1, answer[1]+1]
