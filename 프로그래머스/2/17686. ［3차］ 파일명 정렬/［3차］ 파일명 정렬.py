9c14cd8a3b8d90a15891f9794e3bb0abcbf4894d
정윤 <sju08227@naver.com>
2025-10-06 18:42:47 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 4.67 ms, Memory: 9.52 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,21 +1,28 @@
 def solution(files):
     answer = []
     
-    new = []
-    for file in files:
-        head, num, tail = '', '', ''
-        is_num = False
-        for i in range(len(file)):
-            if file[i].isdigit():
-                num += file[i]
-                is_num = True
-            elif not is_num:
-                head += file[i]
-            else:
-                tail += file[i:]
+    original_file = {}
+    after_file = []
+    
+    for idx, file in enumerate(files):
+        head_end = -1
+        number_start = -1
+        number_end = -1
+
+        for i, ch in enumerate(file):
+            if ch.isdigit():
+                if number_start == -1:
+                    number_start = i
+                    head_end = i - 1
+                number_end = i
+            elif number_start != -1:
                 break
-                
-        new.append((head, num, tail))
         
-    new.sort(key=lambda x: (x[0].upper(), int(x[1])))
-    return [''.join(t) for t in new]
+        head = file[0:head_end+1].lower()
+        number = file[head_end+1:number_end+1]
+        tail = file[number_end+1:]
+        after_file.append([head, int(number), idx, file])
+    
+    after_file.sort(key = lambda x: (x[0], x[1], x[2]))
+    print(after_file)
+    return [x[3] for x in after_file]
