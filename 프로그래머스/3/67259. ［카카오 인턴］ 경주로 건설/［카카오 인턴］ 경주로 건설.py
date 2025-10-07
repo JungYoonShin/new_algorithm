8597630e5fb174f9c670e6645335ec1addf8992a
정윤 <sju08227@naver.com>
2025-10-07 16:48:13 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 19.21 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,37 +1,41 @@
 from collections import deque
-answer = 1e9
-
 def solution(board):
+    answer = 1e9
+    #경주로를 건설하는 데 필요한 최소 비용
+    #0빈칸 1벽 / 직선 -> 100원, 코너 -> 500원
     
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     
     n = len(board)
     
-    visited = [[False for i in range(n)] for _ in range(n)]
-    graph = [[1e9 for _ in range(n)] for _ in range(n)]
-
     def bfs(d):
-        q = deque([(0, 0, d, 0)])
-        graph[0][0] = 0
+        q = deque([(0, 0, 0, d)])
+        total = [[1e9 for _ in range(n)] for _ in range(n)]
+        total[0][0] = 0
         
         while q:
-            a, b, direction, cost = q.popleft()
+            x, y, cost, direction = q.popleft()
             
+#             if x == n-1 and y == n-1:
+#                 answer = min(answer, cost)
+#                 return
+
             for i in range(4):
-                nx = a + dx[i]
-                ny = b + dy[i]
-                
+                nx = x + dx[i]
+                ny = y + dy[i]
+
                 if 0<=nx<n and 0<=ny<n:
                     if board[nx][ny] == 0:
-                        if direction == i:
-                            new_cost = cost + 100
-                        if direction != i:
+                        if i == direction:
+                            new_cost = cost+ 100
+                        else:
                             new_cost = cost + 600
-                        if new_cost <= graph[nx][ny]:
-                            graph[nx][ny] = new_cost
-                            q.append((nx, ny, i, new_cost))
-        
-        return graph[n-1][n-1]
+                        
+                        if new_cost < total[nx][ny]:
+                            total[nx][ny] = new_cost
+                            q.append((nx, ny, new_cost, i))
+        return total[-1][-1]
     
-    return min(bfs(3), bfs(1))
+    answer = min(bfs(1), bfs(3))
+    return answer
