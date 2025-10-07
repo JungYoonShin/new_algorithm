1dee882f446861a33696ef6516c50c501822016e
정윤 <sju08227@naver.com>
2025-10-07 22:04:25 +0900

    [level 3] Title: 합승 택시 요금, Time: 1227.67 ms, Memory: 15.1 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,40 +1,42 @@
 import heapq
 def solution(n, s, a, b, fares):
-    INF = 1e9
-    
+    answer = 0
     graph = [[] for _ in range(n+1)]
     for fare in fares:
-        graph[fare[0]].append([fare[1], fare[2]])
-        graph[fare[1]].append([fare[0], fare[2]])
+        c, d, f = fare
+        graph[c].append([d, f])
+        graph[d].append([c, f])
         
-        
+    
     def djk(start):
-        dist = [INF] * (n+1)
-        dist[start] = 0
         q = []
         heapq.heappush(q, (0, start))
+        dist = [1e9] * (n+1)
+        dist[start] = 0
         
         while q:
-            cost, v = heapq.heappop(q)
-            if dist[v] < cost:
+            cost, now = heapq.heappop(q)
+            
+            if dist[now] < cost:
                 continue
             
-            for togo, w in graph[v]:
-                if dist[v] + w < dist[togo]:
-                    dist[togo] = dist[v] + w
-                    heapq.heappush(q, [dist[togo], togo])
+            for v in graph[now]:
+                if cost + v[1] < dist[v[0]]:
+                    dist[v[0]] = dist[now] + v[1]
+                    heapq.heappush(q, (dist[v[0]], v[0]))
         return dist
-
     
-    together = djk(s)
-    alone_a = together[a]
-    alone_b = together[b]
+    #s에서 각 지점까지의 최단거리를 구한다.
+    dist_s = djk(s)
+    alone_a = dist_s[a]
+    alone_b = dist_s[b]
     
     answer = alone_a + alone_b
+    
     for i in range(1, n+1):
-        #i까지 합승을 하는 거임, 합승을 안할 수도 잇음..(각자 가는게 더 빠르다면)
+        together = dist_s[i]
         alone = djk(i)
-        total = together[i] + alone[a] + alone[b]
-        answer = min(total, answer)
-                
+        total = together + alone[a] + alone[b]
+        answer = min(answer, total)
+    
     return answer
