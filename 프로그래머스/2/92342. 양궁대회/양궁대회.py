ddd94eddc3aa4c9b758c289270504a2795b0f1a8
정윤 <sju08227@naver.com>
2025-10-07 22:32:18 +0900

    [level 2] Title: 양궁대회, Time: 5.17 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -1,44 +1,43 @@
 def solution(n, info):
     answer = []
     
-    # a=b일 경우 어피치가 점수 가져감
-    # a=b=0이면 아무도 안가져감
-    # 라이언이 가장 큰 점수 차이로 우승할 수 있는 방법 return 
     
-    def dfs(idx, ryan, apeach, arrow):
-        if idx == 11:
+    #a=b=0일 경우 아무도 점수 가져가지 x, a=b일 경우 어피치가 점수 가져감
+    #최종 점수 같을 경우 어피치 우승자
+    #라이언이 가장 큰 점수차로 우승하기 위한 배열
+    
+    def dfs(depth, ryan, apeach, arrow):
+        if depth == 11:
             if arrow > 0:
                 ryan[10] += arrow
-            apeach_score, ryan_score = 0, 0
+            ryan_total, apeach_total = 0, 0
             for i in range(11):
-                if apeach[i] == 0 and ryan[i] == 0:
+                if ryan[i] == apeach[i] == 0:
                     continue
-                if apeach[i] < ryan[i]:
-                    ryan_score += (10-i)
+                if ryan[i] > apeach[i]:
+                    ryan_total += (10-i)
                 else:
-                    apeach_score += (10-i)
+                    apeach_total += (10-i)
             
-            if ryan_score > apeach_score:
-                answer.append([ryan_score-apeach_score, ryan[:]])
-            
+            if apeach_total < ryan_total:
+                answer.append([ryan_total-apeach_total, ryan[:]])
+                
             if arrow > 0:
                 ryan[10] -= arrow
-                
+            
             return
         
-        #해당 점수 먹거나
-        need = apeach[idx] + 1
+        #해당 점수 따거나
+        need = apeach[depth] + 1
         if arrow >= need:
-            ryan[idx] = need
-            dfs(idx+1, ryan, apeach, arrow-need)
-            ryan[idx] = 0
-            
+            ryan[depth] = need
+            dfs(depth+1, ryan, apeach, arrow-need)
+            ryan[depth] = 0
         
-        #안 먹거나
-        dfs(idx+1, ryan, apeach, arrow)
+        #헤당 점수 안 따거나
+        dfs(depth+1, ryan, apeach, arrow)
     
-    dfs(0, [0]*11, info, n)
-    
+    dfs(0, [0] * 11, info, n)
     if not answer:
         return [-1]
     else:
