8ebee937fd4cdbea32fa6dd1f9b79ddac2c14aa6
정윤 <sju08227@naver.com>
2025-10-14 18:18:37 +0900

    [Gold I] Title: 구슬 탈출, Time: 56 ms, Memory: 35068 KB -BaekjoonHub

--- 백준/Gold/13459. 구슬 탈출/구슬 탈출.py
+++ 백준/Gold/13459. 구슬 탈출/구슬 탈출.py
@@ -1,66 +1,68 @@
 import sys
+input = sys.stdin.readline
 from collections import deque
 
-input = sys.stdin.readline
 
+#파란 구슬을 구멍에 넣지 않으면서 빨간 구슬을 10번 이하로 움직여서 빼낼 수 있으면 1
 n, m = map(int, input().split())
-r, c = n, m
+board = [list(input().rstrip()) for _ in range(n)]
 
-graph = [list(input().rstrip()) for _ in range(r)]
-
-for i in range(r):
-    for j in range(c):
-        if graph[i][j] == 'R':
-            red_x, red_y = i, j
-        elif graph[i][j] == 'B':
-            blue_x, blue_y = i, j
-
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == 'R':
+            rx, ry = i, j
+        if board[i][j] == 'B':
+            bx, by = i, j
 dx = [-1, 1, 0, 0]
-dy = [0, 0, 1, -1]
-
-def move(x, y, dx, dy):
-    count = 0
-    while graph[x+dx][y+dy] != '#' and graph[x][y] != 'O':
-        x += dx
-        y += dy
-        count += 1
-        if graph[x][y] == 'O':
+dy = [0, 0, -1, 1]
+def move(d, x, y):
+    cnt = 0
+    while True:
+        nx = x + dx[d]
+        ny = y + dy[d]
+        if board[nx][ny] == '#':  # 벽이면 멈춤
             break
-    return x, y, count
+        if board[nx][ny] == 'O':  # 구멍이면 좌표만 반환
+            return nx, ny, cnt
+        x, y = nx, ny
+        cnt += 1
+    return x, y, cnt
 
-def bfs():
-    q = deque()
-    q.append((red_x, red_y, blue_x, blue_y, 0))
+def bfs(a, b, c, d, cnts):
+    q = deque([(a, b, c, d, cnts)])
     visited = set()
-    visited.add((red_x, red_y, blue_x, blue_y))
+    visited.add((a, b, c, d))
 
     while q:
-        rx, ry, bx, by, depth = q.popleft()
-        if depth >= 10:
-            continue
+        rx, ry, bx, by, cnt = q.popleft()
 
-        for k in range(4):
-            nrx, nry, rc = move(rx, ry, dx[k], dy[k])
-            nbx, nby, bc = move(bx, by, dx[k], dy[k])
+        if cnt >= 11:
+            return 0
 
-            if graph[nbx][nby] == 'O':
+        for d in range(4):
+            nrx, nry, depthr = move(d, rx, ry)
+            nbx, nby, depthb = move(d, bx, by)
+
+            if board[nbx][nby] == 'O':
                 continue
-                # 빨간 공만 구멍에 들어가면 성공
-            if graph[nrx][nry] == 'O':
+
+            if board[nrx][nry] == 'O':
                 return 1
 
-            if nrx == nbx and nry == nby:
-                if rc > bc:
-                    nrx -= dx[k]
-                    nry -= dy[k]
+            #둘이 같은 칸에 못있게 함
+            if (nrx == nbx) and (nry == nby):
+                if depthr > depthb:
+                    nrx -= dx[d]
+                    nry -= dy[d]
                 else:
-                    nbx -= dx[k]
-                    nby -= dy[k]
+                    nbx -= dx[d]
+                    nby -= dy[d]
 
-            if (nrx, nry, nbx, nby) not in visited:
-                visited.add((nrx, nry, nbx, nby))
-                q.append((nrx, nry, nbx, nby, depth+1))
+            if board[nrx][nry] != 'O' and board[nbx][nby] != 'O':
+                if (nrx, nry, nbx, nby) not in visited:
+                    visited.add((nrx, nry, nbx, nby))
+                    q.append((nrx, nry, nbx, nby, cnt+1))
+
     return 0
 
-
-print(bfs())
+print(bfs(rx, ry, bx, by, 1))
