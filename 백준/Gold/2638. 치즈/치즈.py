54b82c7b62027bdbdfbc115bbb5500f8bdcd6999
정윤 <sju08227@naver.com>
2025-10-19 16:13:20 +0900

    [Gold III] Title: 치즈, Time: 624 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,51 +1,41 @@
-import sys
-sys.setrecursionlimit(10**6)
-
+from collections import deque
 n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-# 외부공기 표시
-def dfs(x, y, visited):
-    visited[x][y] = True
-    for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m:
-            if not visited[nx][ny]:
-                if board[nx][ny] == 0:
-                    board[nx][ny] = 2  # 외부공기 표시
-                    dfs(nx, ny, visited)
-                elif board[nx][ny] == 2:
-                    dfs(nx, ny, visited)
 
-def melt():
-    global board
-    new_board = [row[:] for row in board]
-    melted = False
-    for i in range(n):
-        for j in range(m):
-            if board[i][j] == 1:
-                cnt = 0
-                for k in range(4):
-                    nx, ny = i + dx[k], j + dy[k]
-                    if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 2:
-                        cnt += 1
-                if cnt >= 2:
-                    new_board[i][j] = 0
-                    melted = True
-    board = new_board
-    return melted
+def bfs():
+    q = deque([(0, 0)])
+    visited = [[0 for _ in range(m)] for _ in range(n)]
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0<=nx<n and 0<=ny<m:
+                if board[nx][ny] == 0 and not visited[nx][ny]:
+                    visited[nx][ny] = 1
+                    q.append((nx, ny))
+                if board[nx][ny] == 1:
+                    visited[nx][ny] += 1
+    return visited
 
 time = 0
 while True:
-    # 외부공기 새로 갱신
-    visited = [[False]*m for _ in range(n)]
-    dfs(0, 0, visited)
-
-    # 치즈 녹이기
-    if not melt():
-        break
+    visited = bfs()
+    for i in range(n):
+        for j in range(m):
+            if visited[i][j] >= 2:
+                board[i][j] = 0
+    flag = False
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == 1:
+                flag = True
     time += 1
-
+    if not flag:
+        break
 print(time)
