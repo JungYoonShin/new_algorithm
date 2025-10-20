a0bd2e5c762a970d4dbdb7eecfbdbec22f6258dd
정윤 <sju08227@naver.com>
2025-10-20 19:10:41 +0900

    [Gold V] Title: MooTube (Silver), Time: 5012 ms, Memory: 35544 KB -BaekjoonHub

--- 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
@@ -1,4 +1,14 @@
+import sys
 from collections import deque, defaultdict
+input = sys.stdin.readline
+
+n, q = map(int, input().split())
+graph = defaultdict(list)
+
+for _ in range(n - 1):
+    a, b, usado = map(int, input().split())
+    graph[a].append((b, usado))
+    graph[b].append((a, usado))
 
 def bfs(k, v):
     visited = [0] * (n + 1)
@@ -6,22 +16,16 @@ def bfs(k, v):
     result = 0
     queue = deque([(v, 1e9)])
     while queue:
-        v, usado = queue.pop()
+        v, usado = queue.popleft()
         for n_v, n_usado in graph[v]:
             n_usado = min(usado, n_usado)
             if n_usado >= k and not visited[n_v]:
                 result += 1
                 queue.append((n_v, n_usado))
                 visited[n_v] = 1
+
     return result
 
-
-n, q = map(int, input().split())
-graph = defaultdict(list)
-for _ in range(n - 1):
-    a, b, w = map(int, input().split())
-    graph[a].append((b, w))
-    graph[b].append((a, w))
 for _ in range(q):
     k, v = map(int, input().split())
     print(bfs(k, v))
