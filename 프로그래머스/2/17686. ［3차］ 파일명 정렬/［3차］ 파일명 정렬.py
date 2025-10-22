fcb8c8cf3368a8dffdf53c3315198f197a1e82b9
정윤 <sju08227@naver.com>
2025-10-22 17:14:44 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 5.50 ms, Memory: 9.76 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,21 +1,32 @@
 def solution(files):
     answer = []
     
-    new = []
-    for file in files:
-        head, num, tail = '', '', ''
-        is_num = False
-        for i in range(len(file)):
-            if file[i].isdigit():
-                num += file[i]
-                is_num = True
-            elif not is_num:
-                head += file[i]
-            else:
-                tail += file[i:]
+    #head, number, tail 3부분 
+    #number는 최초로 숫자가 등장한 곳(최대 1-5글자의 숫자)
+    for k, file in enumerate(files):
+        number_appear = False
+        number_start = 0
+        number_end = 0
+        for i, f in enumerate(file):
+            if not number_appear and f.isdigit():
+                number_start = i
+                number_end = i
+                number_appear = True
+            elif number_appear and f.isdigit() and i - number_start < 5:
+                number_end = i
+            elif number_appear and not f.isdigit():
                 break
-                
-        new.append((head, num, tail))
+            
+
+        head = file[0:number_start]
+        if head:
+            head = head.lower()
+        number = int(file[number_start:number_end+1])
+        tail = file[number_end+1:]
+        answer.append([file, head, number, k])
         
-    new.sort(key=lambda x: (x[0].upper(), int(x[1])))
-    return [''.join(t) for t in new]
+        print(head, number, tail)
+
+    answer.sort(key=lambda x: (x[1], x[2], x[3]))
+    answer = [x[0] for x in answer]
+    return answer
