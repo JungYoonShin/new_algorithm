6e4f9732fdd33e83e183784d3b77b6cd27dd36e1
정윤 <sju08227@naver.com>
2025-10-22 18:23:53 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.43 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,38 +1,40 @@
+from collections import deque
 def solution(places):
     answer = []
+    
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-    
-    #응시자들끼리는 맨해튼 거리가 2이하면 안된다.
-    def dfs(place, x, y, depth):
-        if place[x][y] == 'P' and 1<= depth <= 2:
-            return True
+    def bfs(i, j):
+        q = deque([(i, j, 0)])
+        visited = [[False for _ in range(5)] for _ in range(5)]
         
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        while q:
+            a, b, depth = q.popleft()
+            visited[a][b] = True
+            if place[a][b] == 'P' and 1<=depth<=2:
+                return 0
+            for i in range(4):
+                nx = a + dx[i]
+                ny = b + dy[i]
+                
+                if 0<=nx<5 and 0<=ny<5:
+                    if not visited[nx][ny] and place[nx][ny] != 'X':
+                        q.append((nx, ny, depth+1))
             
-            if 0<=nx<5 and 0<=ny<5:
-                if not visited[nx][ny] and place[nx][ny] != 'X':
-                    visited[nx][ny] = True
-                    if dfs(place, nx, ny, depth+1):
-                        return True
-                    visited[nx][ny] = False
-                    
+        return 1
+    
     for place in places:
         flag = False
         for i in range(5):
             for j in range(5):
                 if place[i][j] == 'P':
-                    visited = [[False for _ in range(5)] for _ in range(5)]
-                    visited[i][j] = True
-                    #거리두기를 지키지 않고 있는 경우
-                    if dfs(place, i, j, 0):
-                        answer.append(0)
+                    if bfs(i, j) == 0:
                         flag = True
+                        answer.append(0)
                         break
             if flag:
                 break
         if not flag:
             answer.append(1)
+        
     return answer
