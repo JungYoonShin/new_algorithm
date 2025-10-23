ea2ec95b7a006a45488a25e0dcbd4e4cc3a9edfa
정윤 <sju08227@naver.com>
2025-10-23 18:28:48 +0900

    [level 2] Title: 지게차와 크레인, Time: 229.50 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.py
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.py
@@ -1,77 +1,58 @@
 from collections import deque
-
 def solution(storage, requests):
     answer = 0
+    
+    #알파벳 하나 -> 접근 가능한 컨테이너(4면 중 적어도 1면이 외부와 연결된 컨테이너)만 꺼낸다.
+    #알파벳 두개 -> 알파벳에 해당하는 모든 컨테이너 꺼낸다. 
+    
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
     n = len(storage)
     m = len(storage[0])
-    
     storage = list(map(list, storage))
     new_storage = [[0 for _ in range(m+2)] for _ in range(n+2)]
     for i in range(n):
         for j in range(m):
             new_storage[i+1][j+1] = storage[i][j]
-    
+
     storage = new_storage
-    
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    
-    def out_side():
+    def bfs(alphabet):
         q = deque([(0, 0)])
         visited = [[False for _ in range(m+2)] for _ in range(n+2)]
         visited[0][0] = True
         
         while q:
             a, b = q.popleft()
-            
+                
             for i in range(4):
                 nx = a + dx[i]
                 ny = b + dy[i]
                 
                 if 0<=nx<n+2 and 0<=ny<m+2:
-                    if not visited[nx][ny]:
-                        if storage[nx][ny] == -1:
-                            storage[nx][ny] = 0
-                            q.append((nx, ny))
-                            visited[nx][ny] = True
-                        elif storage[nx][ny] == 0:
-                            q.append((nx, ny))
-                            visited[nx][ny] = True
-    
-    def get_out(request):
-        alphabet = request[0]
-        if len(request) == 1:
-            remove = []
-            for i in range(1, n+1):
-                for j in range(1, m+1):
-                    if storage[i][j] == alphabet:
-                        for k in range(4):
-                            nx = i + dx[k]
-                            ny = j + dy[k]
-                            if storage[nx][ny] == 0:
-                                remove.append((i, j))
-                                break
-            for a, b in remove:
-                storage[a][b] = 0
-            out_side()
-                
-        elif len(request) == 2:
-            for i in range(1, n+1):
-                for j in range(1, m+1):
-                    if storage[i][j] == alphabet:
-                        storage[i][j] = -1
-            
-            out_side()
-        
-    
+                    if storage[nx][ny] == alphabet:
+                        outside.append((nx, ny))
+                    if not visited[nx][ny] and storage[nx][ny] == 0:
+                        visited[nx][ny] = True
+                        q.append((nx, ny))
+                        
     for request in requests:
-        get_out(request)
-        # print(*storage, sep= "\n")
-        # print()
+        alphabet = request[0]
+        outside = []
+        if len(request) == 1:
+            bfs(alphabet)
+            for a, b in outside:
+                storage[a][b] = 0
+            
         
+        else:
+            for i in range(1, n+1):
+                for j in range(1, m+1):
+                    if storage[i][j] == alphabet:
+                        storage[i][j] = 0
+    
     for i in range(1, n+1):
         for j in range(1, m+1):
-            if storage[i][j] != 0 and storage[i][j] != -1:
+            if storage[i][j] != 0:
                 answer += 1
-    
+            
     return answer
