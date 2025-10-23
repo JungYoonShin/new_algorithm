a5fff0fc408056d55a08fbe7c0a70738a87031b3
정윤 <sju08227@naver.com>
2025-10-23 23:25:01 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 73.56 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/README.md
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 보석 쇼핑 - 67258 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67258?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67258) 
 
 ### 성능 요약
 
-메모리: 16.8 MB, 시간: 72.86 ms
+메모리: 16.8 MB, 시간: 73.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 06일 15:36:13
+2025년 10월 23일 23:24:59
 
 ### 문제 설명
 
--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,28 +1,24 @@
 from collections import defaultdict
 def solution(gems):
-    answer = []
+    answer = [0, 1e9]
     
-    find_gem = defaultdict(int)
-    start = 0
+    diamond = len(set(gems))
     n = len(gems)
-    gem_type = len(set(gems))
-    answer = [0, n-1]
-    unique = 0
     end = 0
+    status = defaultdict(int)
+    
     
     for start in range(n):
-        while unique < gem_type and end < n:
-            find_gem[gems[end]] += 1
-            if find_gem[gems[end]] == 1:
-                unique += 1
+        while end < n and len(status) < diamond:
+            status[gems[end]] += 1
             end += 1
+            
+        if len(status) == diamond and end-1-start < answer[1] - answer[0]:
+            answer = [start+1, end]
         
-        if unique == gem_type:
-            if end-1-start < answer[1] - answer[0]:
-                answer= [start, end-1]
-        
-        find_gem[gems[start]] -=1
-        if find_gem[gems[start]] == 0:
-            unique -= 1
-    
-    return [answer[0]+1, answer[1]+1]
+        status[gems[start]] -= 1
+        if status[gems[start]] == 0:
+            del status[gems[start]]
+        start += 1
+            
+    return answer
