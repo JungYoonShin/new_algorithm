09af7f948a876d36f5e2e3ddd7dfc8dd2dda7677
정윤 <sju08227@naver.com>
2025-10-25 00:53:01 +0900

    [level 2] Title: 순위 검색, Time: 1090.18 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.py
@@ -1,31 +1,29 @@
-import copy
-from bisect import bisect_left
 from itertools import combinations
+from bisect import bisect_left
 from collections import defaultdict
 def solution(infos, query):
     answer = []
     
-    all_info = defaultdict(list)
+    status = defaultdict(list)
     for info in infos:
-        q = info.split(" ")
-        for x in range(5):
-            #- 등장은 0부터 4번가능
-            cases = list(combinations([0, 1, 2, 3], x))
-            for case in cases:
-                temp_q = copy.deepcopy(q)
-                for c in case:
-                    temp_q[c] = '-'
-            
-                all_info[''.join(temp_q[0:4])].append(int(q[-1]))
-        
-    for info in all_info.keys():
-        all_info[info].sort()
-        
+        #4자리 중 0~4개가 -가 들어갈 수 있다.
+        for i in range(5):
+            places = list(combinations(range(4), i))
+            for place in places:
+                new = info.split(" ")
+                score = new[4]
+                new = new[0:4]
+                for p in place:
+                    new[p] = '-'
+                status[''.join(new)].append(int(score))
+                
+    for k in status.keys():
+        status[k].sort()
+    
     for q in query:
-        q = q.replace(' and ' , '')
+        q = q.replace(' and ', ' ')
         q = q.split(' ')
-        score = int(q[-1])
-        idx = bisect_left(all_info[q[0]], score)
-        answer.append(len(all_info[q[0]]) - idx)
-
+        score = int(q[4])
+        q = ''.join(q[0:4])
+        answer.append(len(status[q]) - bisect_left(status[q], score))
     return answer
