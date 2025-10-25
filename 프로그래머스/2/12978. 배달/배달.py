2bb7e2ebe9683c3d6534cdec0123586ab41de492
정윤 <sju08227@naver.com>
2025-10-25 21:55:26 +0900

    [level 2] Title: 배달, Time: 1.77 ms, Memory: 9.8 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,35 +1,33 @@
 import heapq
 def solution(N, road, K):
     answer = 0
-    n = N
-    #1번 마을에 있는 음식점이 K 이하의 시간에 배달이 가능한 마을의 개수를 return
-    graph = [[] for _ in range(n+1)]
     
-    for a, b, c in road:
-        graph[a].append([b, c])   #이어진 노드, 가중치
+    graph = [[] for _ in range(N+1)]
+    #1번 마을에 있는 음식점이 K 이하의 시간에 배달이 가능한 마을의 개수
+    for a,b,c in road:
+        graph[a].append([b, c])
         graph[b].append([a, c])
     
     def djk():
         q = []
-        heapq.heappush(q, [0, 1])  #cost, 현재 노드
-        dist = [1e9] * (n+1)
+        heapq.heappush(q, (0, 1)) #cost, 시작점
+        dist = [1e9] * (N+1)
         dist[1] = 0
         
         while q:
             cost, now = heapq.heappop(q)
-            
             if dist[now] < cost:
                 continue
             
             for v in graph[now]:
                 if dist[now] + v[1] < dist[v[0]]:
                     dist[v[0]] = dist[now] + v[1]
-                    heapq.heappush(q, [dist[v[0]], v[0]])
+                    heapq.heappush(q, (dist[v[0]], v[0]))
         return dist
     
-    result = djk()
-    for i, v in enumerate(result):
-        if v <= K:
+    dist = djk()
+    for i in range(1, N+1):
+        if dist[i] <= K:
             answer += 1
-
+    # print(dist)
     return answer
