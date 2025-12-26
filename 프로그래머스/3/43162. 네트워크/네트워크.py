eb29e4bc542133fcbee668d47260175cd0ebe10b
정윤 <sju08227@naver.com>
2025-12-26 22:25:10 +0900

    [level 3] Title: 네트워크, Time: 0.68 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,17 +1,18 @@
 def solution(n, computers):
     answer = 0
-    
-    def dfs(x):
-        visited[x] = True
-        
-        for i in range(n):
-            if computers[x][i] and not visited[i]:
-                dfs(i)
-    
     visited = [0] * n
+    graph = [[] for _ in range(n)]
+        
+    def dfs(v):
+        visited[v] = True
+        for i in range(n):
+            if i != n and computers[v][i] == 1:
+                if not visited[i]:
+                    dfs(i)
+        
     for i in range(n):
         if not visited[i]:
             dfs(i)
             answer += 1
-    
+        
     return answer
