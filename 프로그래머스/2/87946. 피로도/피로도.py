45d7f5987a9ad4a28793fef608df548654261684
정윤 <sju08227@naver.com>
2025-12-29 20:03:36 +0900

    [level 2] Title: 피로도, Time: 39.56 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,24 +1,21 @@
-answer = -1
 def solution(k, dungeons):
+    answer = -1
     n = len(dungeons)
+    visited = [False] * n
     
-    #탐험할 수 있는 최대 던전 수
-    def dfs(depth, cost, visit):
-        global answer
-        answer = max(answer, depth)
-        if depth == n:
-            print(visit)
-            return
-
+    def explore(visit, left):   
+        nonlocal answer
         for i in range(n):
-            #방문하지 않은 던전이라면
-            if i not in visit and cost >= dungeons[i][0]:
+            if not visited[i] and left >= dungeons[i][0]:
+                visited[i] = True
                 visit.append(i)
-                cost -= dungeons[i][1]
-                dfs(depth+1, cost, visit)
-                visit.pop() 
-                cost += dungeons[i][1]
-                
-    visit = []
-    dfs(0, k, visit)
+                explore(visit, left-dungeons[i][1])
+                visit.pop()
+                visited[i] = False
+        
+        if answer < len(visit):
+            answer = len(visit)
+    
+    explore([], k)
+        
     return answer
