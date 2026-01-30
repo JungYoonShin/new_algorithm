da7483be0324d3ea5c6b8833ed7988d21f024b26
정윤 <sju08227@naver.com>
2026-01-30 10:21:57 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.53 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,40 +1,36 @@
 from collections import deque
 def solution(places):
     answer = []
-    
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-    def bfs(i, j):
-        q = deque([(i, j, 0)])
+    
+    def bfs(x, y, place):
+        q = deque([(x, y, 0)])
         visited = [[False for _ in range(5)] for _ in range(5)]
-        
+        visited[x][y] = True
         while q:
-            a, b, depth = q.popleft()
-            visited[a][b] = True
-            if place[a][b] == 'P' and 1<=depth<=2:
-                return 0
-            for i in range(4):
-                nx = a + dx[i]
-                ny = b + dy[i]
-                
+            a, b, dist = q.popleft()
+            if 0<dist<=2 and place[a][b] == 'P':
+                return False
+            for d in range(4):
+                nx, ny = a + dx[d], b + dy[d]
                 if 0<=nx<5 and 0<=ny<5:
                     if not visited[nx][ny] and place[nx][ny] != 'X':
-                        q.append((nx, ny, depth+1))
-            
-        return 1
+                        q.append((nx, ny, dist+1))
+                        visited[nx][ny] = True
+        return True
     
     for place in places:
         flag = False
         for i in range(5):
             for j in range(5):
                 if place[i][j] == 'P':
-                    if bfs(i, j) == 0:
-                        flag = True
+                    if not bfs(i, j, place):
                         answer.append(0)
+                        flag = True
                         break
             if flag:
                 break
         if not flag:
             answer.append(1)
-        
     return answer
