ab636cd00a9dfe6301f7c00a059f797151b75e9d
정윤 <sju08227@naver.com>
2026-02-16 21:08:25 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,6 @@
+-- 코드를 작성해주세요
+select ID, EMAIL, FIRST_NAME, LAST_NAME
+from developers 
+where skill_code & (select code from skillcodes where name = 'C#') 
+or skill_code & (select code from skillcodes where name = 'Python') 
+order by id asc
