060ec7043668d503943c3045c6eb9c626803d6e1
정윤 <sju08227@naver.com>
2026-02-19 17:47:52 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.12 ms, Memory: 56.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 54.7 MB, 시간: 10.01 ms
+메모리: 56.5 MB, 시간: 10.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 02일 16:02:19
+2026년 02월 19일 17:47:49
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,36 +1,41 @@
 import java.util.*;
 class Solution {
-    public int solution(int[][] maps) {
-        return bfs(maps);
-    }
+    
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
     
     int bfs(int[][] maps) {
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
         int n = maps.length;
         int m = maps[0].length;
-        Queue<int[]> q = new ArrayDeque<>();
+        
         boolean[][] visited = new boolean[n][m];
+        ArrayDeque<int[]> q = new ArrayDeque<>();
         q.add(new int[]{0, 0, 1});
         visited[0][0] = true;
-        
-        while(!q.isEmpty()){
-            int now[] = q.poll();
-            if (now[0] == n-1 && now[1] == m-1){
-                return now[2];
+        int[] status = null;
+        boolean flag = false;
+        while(!q.isEmpty()) {
+            status = q.poll();
+            if(status[0] == n-1 && status[1] == m-1) {
+                return status[2];
             }
-            
             for(int i=0; i<4; i++) {
-                int nx = now[0] + dx[i];
-                int ny = now[1] + dy[i];
+                int nx = status[0] + dx[i];
+                int ny = status[1] + dy[i];
                 
-                if ((nx>=0  && nx<n && ny>=0 && ny<m) && !visited[nx][ny] && maps[nx][ny] != 0) {
-                    visited[nx][ny] = true;
-                    q.add(new int[]{nx, ny, now[2] + 1});
+                if(0<=nx && nx<n && 0<=ny && ny < m){
+                    if(maps[nx][ny] == 1 && visited[nx][ny] == false) {
+                        visited[nx][ny] = true;
+                        q.add(new int[]{nx, ny, status[2]+1});
+                    }
                 }
             }
         }
-        
         return -1;
+    }
+    
+    public int solution(int[][] maps) {
+        int answer = 0;
+        return bfs(maps);
     }
 }
